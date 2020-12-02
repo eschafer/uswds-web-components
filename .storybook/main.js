7b386843387616f5040fe193207eef5cc9b9fc62
Elizabeth Schafer <1136332+eschafer@users.noreply.github.com>
2020-12-02 02:06:58 -0500

    Added a story that displays the project's readme

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    {
+      name: '@storybook/addon-docs',
+      options: { transcludeMarkdown: true },
+    },
+    "@storybook/addon-essentials",
   ]
 }
